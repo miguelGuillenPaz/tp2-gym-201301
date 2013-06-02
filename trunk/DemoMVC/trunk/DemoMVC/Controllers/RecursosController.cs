bc6a945d02122a537cecba675e492c047f5e0965
kgoytizolo@gmail.com <kgoytizolo@gmail.com@c1a021b2-a583-5a13-b94e-629bbfb02de1>
2013-06-02 23:48:25 +0000

    Commit para Subversion

---
+++ trunk/DemoMVC/trunk/DemoMVC/Controllers/RecursosController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoMVC.Controllers
+{
+    public class RecursosController : Controller
+    {
+        //
+        // GET: /Recursos/
+
+        public ActionResult Recursos()
+        {
+            return View();
+        }
+
+    }
+}
