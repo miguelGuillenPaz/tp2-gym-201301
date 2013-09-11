99bc77b3c40f321bb02c50a8ad8b62476b2823cd
schattentod@gmail.com <schattentod@gmail.com@c1a021b2-a583-5a13-b94e-629bbfb02de1>
2013-09-11 07:32:19 +0000

    Repository de Presupuesto

---
+++ DemoMVC/trunk/DemoMVC/Repositories/IGPCRepository/IGPCDetallePartidaRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GYM.SIC.GPC.Models;
+using GYM.SIC.GPC.Repositories.S10Repository;
+using DemoMVC.Models;
+
+namespace GYM.SIC.GPC.Repositories.GPCRepository
+{
+    public interface IGPCDetallePartidaRepository
+    {
+        IQueryable<GPC_DetallePartida> DetallesPartida { get; }
+        IQueryable<GetDetallesPartida_Result> DetallePartida(string strFechaAvance);
+        IQueryable<GetUsuarios_Result> Usuarios();
+        IQueryable<GPC_AvancePartida> AvancePartida { get; }
+        IQueryable<GetInconsistenciaPartida_Result> InconsistenciaPartida(int idPartida, string fecha);
+        IQueryable<GPC_InconsistenciaAvancePartida> InconsistenciaAvancePartida { get; }
+        GPC_InconsistenciaAvancePartida GetInconsistenciaAvancePartida(int idAvancePartida);
+
+        void CrearAvancePartida(GPC_AvancePartida AvancePartida);
+        void ActualizarInconsistenciaPartida(GPC_InconsistenciaAvancePartida InconsistenciaPartida);
+    }
+}
