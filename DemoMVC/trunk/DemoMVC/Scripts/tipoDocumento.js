355fab1b447d8de39fc36296123103b2395d038a
l.cabrerapillaca@gmail.com <l.cabrerapillaca@gmail.com@c1a021b2-a583-5a13-b94e-629bbfb02de1>
2013-09-12 05:09:46 +0000

    js que tiene funcion: eliminar TipoDocumento.

---
+++ DemoMVC/trunk/DemoMVC/Scripts/tipoDocumento.js
@@ -0,0 +1,49 @@
+﻿$(document).ready(function () {
+
+    $('#tblTipoDocumento').delegate('.eliminar', "click", function () {
+        var tr = $(this).parent().parent();
+        var idTipoDocumento = $.trim($('td:eq(0)', tr).text());
+        if (idTipoDocumento != 0) {
+            $('#dialogConfirm #confirm').text('¿Desea eliminar el Tipo de Documento?');
+            $('#dialogConfirm').dialog({
+                resizable: false,
+                height: 140,
+                
+                modal: true,
+                buttons: {
+                    'Aceptar': function () {
+                        var data = { id: idTipoDocumento };
+                        var url = '/TipoDocumento/Eliminar';
+                        $.ajax({
+                            type: 'POST',
+                            url: url,
+                            data: data,
+                            success: function (result) {
+                                if (result.result) {
+                                    tr.remove();
+                                    var cantActual = parseInt($('#cant').html());
+
+                                    cantActual--;
+                                    __ShowMessage('Registro eliminado satisfactoriamente.');
+                                    $('#cant').html(cantActual);
+                                    
+                                }
+                            },
+                            error: function () {
+                                __ShowMessage('No se pudo eliminar. Este tipo de Documento se encuentra en uso.');
+                            }
+                        });
+                        $(this).dialog('close');
+                    },
+                    'Cancelar': function () {
+                        $(this).dialog('close');
+                    }
+                }
+            });
+        } else {
+            tr.remove();
+        }
+    });
+
+
+});
