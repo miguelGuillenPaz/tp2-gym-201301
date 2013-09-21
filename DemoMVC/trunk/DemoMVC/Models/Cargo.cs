de9c984c1c877d7e48aacc054b48cf1109954b63
kgoytizolo@gmail.com <kgoytizolo@gmail.com@c1a021b2-a583-5a13-b94e-629bbfb02de1>
2013-09-21 18:30:24 +0000

    Se añade modelo usado por Planificacion

---
+++ DemoMVC/trunk/DemoMVC/Models/Cargo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoMVC.Models
+{
+    public class Cargo
+    {
+
+        public Cargo()
+        {
+    
+        }
+
+        public int IdCargo { get; set; }
+        public string Descripcion { get; set; }
+
+    }
+}
