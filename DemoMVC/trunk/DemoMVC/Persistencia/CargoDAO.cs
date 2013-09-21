1cae671a084b9507a39aa99b5205f6321fd256c6
kgoytizolo@gmail.com <kgoytizolo@gmail.com@c1a021b2-a583-5a13-b94e-629bbfb02de1>
2013-09-21 18:35:58 +0000

    Se añade persistencia usada por Planificacion

---
+++ DemoMVC/trunk/DemoMVC/Persistencia/CargoDAO.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Collections;
+using DemoMVC.Models;
+
+namespace DemoMVC.Persistencia
+{
+    public class CargoDAO
+    {
+
+        //Listado de actividades
+        public List<Cargo> obtenerCargo() {
+
+            List<Cargo> listadoCargo = null;
+            Cargo cargo = null;
+            string sql = "select * from dbo.GRH_Cargo";
+
+            using (SqlConnection con = new SqlConnection(ConexionUtil.Cadena))
+            {
+                con.Open();
+                using (SqlCommand com = new SqlCommand(sql, con))
+                {
+                    using (SqlDataReader resultado = com.ExecuteReader())
+                    {
+                        if (resultado.HasRows)
+                        {
+                            //Obtenemos uno por uno
+                            listadoCargo = new List<Cargo>();
+                            while (resultado.Read())
+                            {
+                                //Recorremos objeto por objeto y anadimos
+                                cargo = new Cargo
+                                {
+                                    IdCargo = (int)resultado["IdCargo"],
+                                    Descripcion = (string)resultado["Descripcion"],                                    
+                                };
+                                //Adicionamos al arreglo del objeto
+                                listadoCargo.Add(cargo);
+                            }
+                        }
+                    }
+                }
+                con.Close();
+            }
+
+            return listadoCargo;
+
+        }
+
+    }
+}
