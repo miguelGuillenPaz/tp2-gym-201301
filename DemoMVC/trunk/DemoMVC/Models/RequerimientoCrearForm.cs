e702999b7a21778637db1f666d80a215eb2f2fdb
kgoytizolo@gmail.com <kgoytizolo@gmail.com@c1a021b2-a583-5a13-b94e-629bbfb02de1>
2013-09-21 18:33:21 +0000

    Se añade modelo usado por Planificacion

---
+++ DemoMVC/trunk/DemoMVC/Models/RequerimientoCrearForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web;
+
+namespace DemoMVC.Models
+{
+    public class RequerimientoCrearForm
+    {
+        public RequerimientoCrearForm() { }
+
+        public SelectList ListaProyectos { get; set; }
+
+        public string Fecha { get; set; }
+
+    }
+}
