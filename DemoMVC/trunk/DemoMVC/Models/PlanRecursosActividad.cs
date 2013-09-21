fd1a14262d23c2585b2ceff4d45e35f66e629626
kgoytizolo@gmail.com <kgoytizolo@gmail.com@c1a021b2-a583-5a13-b94e-629bbfb02de1>
2013-09-21 18:30:49 +0000

    Se añade modelo usado por Planificacion

---
+++ DemoMVC/trunk/DemoMVC/Models/PlanRecursosActividad.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoMVC.Models
+{
+    public class PlanRecursosActividad
+    {
+
+        public PlanRecursosActividad() 
+        { 
+        
+        }
+
+        public int IdActividad { get; set; }
+        public String Descripcion { get; set; }
+        public int IdRecurso { get; set; }
+        public String DescRec { get; set; }
+        public Int16 Cantidad { get; set; }
+        public double Horas { get; set; }
+        public int idTipoRecurso { get; set; }
+
+        public string tipoRecurso 
+        {
+            get 
+            {
+                switch (idTipoRecurso) 
+                {
+                    case 1: return "Maquinarias";
+                    case 2: return "Materiales";
+                    case 3: return "Empleados";
+                    default: return "Sin asignar";
+                
+                }
+            }
+        
+        }
+
+    }
+}
