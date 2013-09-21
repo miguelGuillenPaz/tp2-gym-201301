7952f6d8c4b8366889dbb0f17ba881d237b2bb12
kgoytizolo@gmail.com <kgoytizolo@gmail.com@c1a021b2-a583-5a13-b94e-629bbfb02de1>
2013-09-21 18:28:36 +0000

    Cambios de Planificacion

--- DemoMVC/trunk/DemoMVC/Models/Actividad.cs
+++ DemoMVC/trunk/DemoMVC/Models/Actividad.cs
@@ -22,5 +22,54 @@ namespace DemoMVC.Models
 	    public string preAct { get; set; }
 	    public string tipAct { get; set; }
 
+        public string descripcionEntregable { get; set; }
+
+        public int duracion
+        {
+            get
+            {
+
+                DateTime fechaInicio = new DateTime(int.Parse(feciniAct.Substring(6, 4)),
+                                                    int.Parse(feciniAct.Substring(3, 2)),
+                                                    int.Parse(feciniAct.Substring(0, 2)));
+
+                DateTime fechaFin = new DateTime(int.Parse(fecfinAct.Substring(6, 4)),
+                                                 int.Parse(fecfinAct.Substring(3, 2)),
+                                                 int.Parse(fecfinAct.Substring(0, 2)));
+
+                return (int)fechaFin.Subtract(fechaInicio).TotalDays;
+
+            }
+        }
+
+
+
+        public string estado
+        {
+            get
+            {
+
+                if (tipAct != null)
+                {
+
+                    switch (tipAct.Trim())
+                    {
+                        case "0": return "Pendiente";
+                        case "1": return "Cerrado";
+                        default: return "Sin asignar";
+                    }
+
+                }
+                else
+                {
+                    return "Sin asignar";
+
+                }
+
+
+            }
+
+        }
+
     }
 }
