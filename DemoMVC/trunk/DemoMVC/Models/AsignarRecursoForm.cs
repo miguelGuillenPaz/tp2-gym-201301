39cadc7d7e61a50c1190e7d93d9dec96a6ee3f55
kgoytizolo@gmail.com <kgoytizolo@gmail.com@c1a021b2-a583-5a13-b94e-629bbfb02de1>
2013-09-21 18:30:12 +0000

    Se añade modelo usado por Planificacion

---
+++ DemoMVC/trunk/DemoMVC/Models/AsignarRecursoForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoMVC.Models
+{
+    public class AsignarRecursoForm
+    {
+        public AsignarRecursoForm() { }
+
+        public IList<Actividad> ListadoActividad { get; set; }
+
+        public IList<Proyecto> ListadoProyecto { get; set; }
+
+        public IList<Cargo> ListadoCargo { get; set; }
+
+        public IList<Recurso> ListadoEquipos { get; set; }
+
+        public IList<Recurso> ListadoMateriales { get; set; }
+
+        public IList<Recurso> ListadoEmpleados { get; set; }
+
+        public IList<PlanRecursosActividad> ListadoRecursoActividad { get; set; }
+
+    }
+}
