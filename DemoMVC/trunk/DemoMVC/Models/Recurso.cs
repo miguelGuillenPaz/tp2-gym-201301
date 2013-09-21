74020a883ab29a8160d385e3bb0fe34b88f94e2a
kgoytizolo@gmail.com <kgoytizolo@gmail.com@c1a021b2-a583-5a13-b94e-629bbfb02de1>
2013-09-21 18:33:10 +0000

    Se añade modelo usado por Planificacion

---
+++ DemoMVC/trunk/DemoMVC/Models/Recurso.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoMVC.Models
+{
+    public class Recurso
+    {
+
+        public Recurso()
+        { 
+        
+        }
+
+        public int IdRecurso { get; set; }
+        public String Descripcion { get; set; }
+        public double Precio { get; set; }
+        public int IdTipoRecurso { get; set; }
+
+    }
+}
