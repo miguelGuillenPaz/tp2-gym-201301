4c8a3aa5f47d24c48603c653ead6bce7b2b2d286
kgoytizolo@gmail.com <kgoytizolo@gmail.com@c1a021b2-a583-5a13-b94e-629bbfb02de1>
2013-09-21 18:33:34 +0000

    Se añade modelo usado por Planificacion

---
+++ DemoMVC/trunk/DemoMVC/Models/RequerimientoForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoMVC.Models
+{
+    public class RequerimientoForm
+    {
+        public RequerimientoForm() { }
+
+        public Requerimiento Requerimiento { get; set; }
+
+        public SelectList ListadoProyectos { get; set; }
+
+        public IList<DetalleRequerimiento> ListadoDetalleRequerimiento { get; set; }
+
+
+    }
+}
